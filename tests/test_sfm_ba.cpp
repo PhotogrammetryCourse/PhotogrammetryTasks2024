1d38bed4fa2c7729ad37c6a03750c152f84fa000
capitanFlint129 <tekkengod@list.ru>
2024-04-21 11:45:43 +0300

    4.3

--- tests/test_sfm_ba.cpp
+++ tests/test_sfm_ba.cpp
@@ -22,7 +22,7 @@
 #include <ceres/ceres.h>
 
 // TODO включите Bundle Adjustment (но из любопытства посмотрите как ведет себя реконструкция без BA например для saharov32 без BA)
-#define ENABLE_BA                             0
+#define ENABLE_BA                             1
 
 // TODO когда заработает при малом количестве фотографий - увеличьте это ограничение до 100 чтобы попробовать обработать все фотографии (если же успешно будут отрабаывать только N фотографий - отправьте PR выставив здесь это N)
 #define NIMGS_LIMIT                           10 // сколько фотографий обрабатывать (можно выставить меньше чтобы ускорить экспериментирование, или в случае если весь датасет не выравнивается)
@@ -52,6 +52,8 @@
 //#define DATASET_DOWNSCALE            2 // для ускорения SIFT
 //#define DATASET_F                    (2761.5 / DATASET_DOWNSCALE) // see herzjesu25/K.txt
 // TODO почему фокальная длина меняется от того что мы уменьшаем картинку? почему именно в такой пропорции? может надо домножать? или делить на downscale^2 ?
+// При downscale мы уменьшаем плоскость изображения в downscale раз, кажется, что для того чтобы модель камеры оставалась той же самой
+// мы должны усеньшить фокальную длину в том же соотношении иначе область обзора камеры поменяется
 
 // но temple47 - не вышло, я не разобрался в чем с ним проблема, может быть слишком мало точек, может критерии фильтрации выкидышей для него слишком строги
 //#define DATASET_DIR                  "temple47"
@@ -138,14 +140,14 @@ TEST (SFM, ReconstructNViews) {
     std::vector<cv::Mat> imgs;
     std::vector<std::string> imgs_labels;
     {
-        std::ifstream in(std::string("data/src/datasets/") + DATASET_DIR + "/ordered_filenames.txt");
+        std::ifstream in(std::string("/home/flint/s4/Photogrammetry/PhotogrammetryTasks2024/data/src/datasets/") + DATASET_DIR + "/ordered_filenames.txt");
         size_t nimages = 0;
         in >> nimages;
         std::cout << nimages << " images" << std::endl;
         for (int i = 0; i < nimages; ++i) {
             std::string img_name;
             in >> img_name;
-            std::string img_path = std::string("data/src/datasets/") + DATASET_DIR + "/" + img_name;
+            std::string img_path = std::string("/home/flint/s4/Photogrammetry/PhotogrammetryTasks2024/data/src/datasets/") + DATASET_DIR + "/" + img_name;
             cv::Mat img = cv::imread(img_path);
 
             if (img.empty()) {
@@ -282,13 +284,13 @@ TEST (SFM, ReconstructNViews) {
         std::vector<vector3d> tie_points_and_cameras;
         std::vector<cv::Vec3b> tie_points_colors;
         generateTiePointsCloud(tie_points, tracks, keypoints, imgs, aligned, cameras, ncameras, tie_points_and_cameras, tie_points_colors);
-        phg::exportPointCloud(tie_points_and_cameras, std::string("data/debug/test_sfm_ba/") + DATASET_DIR + "/point_cloud_" + to_string(ncameras) + "_cameras.ply", tie_points_colors);
+        phg::exportPointCloud(tie_points_and_cameras, std::string("/home/flint/s4/Photogrammetry/PhotogrammetryTasks2024/data/debug/test_sfm_ba/") + DATASET_DIR + "/point_cloud_" + to_string(ncameras) + "_cameras.ply", tie_points_colors);
 
 #if ENABLE_BA
         runBA(tie_points, tracks, keypoints, cameras, ncameras, calib);
 #endif
         generateTiePointsCloud(tie_points, tracks, keypoints, imgs, aligned, cameras, ncameras, tie_points_and_cameras, tie_points_colors);
-        phg::exportPointCloud(tie_points_and_cameras, std::string("data/debug/test_sfm_ba/") + DATASET_DIR + "/point_cloud_" + to_string(ncameras) + "_cameras_ba.ply", tie_points_colors);
+        phg::exportPointCloud(tie_points_and_cameras, std::string("/home/flint/s4/Photogrammetry/PhotogrammetryTasks2024/data/debug/test_sfm_ba/") + DATASET_DIR + "/point_cloud_" + to_string(ncameras) + "_cameras_ba.ply", tie_points_colors);
     }
 
     // append remaining cameras one by one
@@ -360,7 +362,7 @@ TEST (SFM, ReconstructNViews) {
         std::vector<vector3d> tie_points_and_cameras;
         std::vector<cv::Vec3b> tie_points_colors;
         generateTiePointsCloud(tie_points, tracks, keypoints, imgs, aligned, cameras, ncameras, tie_points_and_cameras, tie_points_colors);
-        phg::exportPointCloud(tie_points_and_cameras, std::string("data/debug/test_sfm_ba/") + DATASET_DIR + "/point_cloud_" + to_string(ncameras) + "_cameras.ply", tie_points_colors);
+        phg::exportPointCloud(tie_points_and_cameras, std::string("/home/flint/s4/Photogrammetry/PhotogrammetryTasks2024/data/debug/test_sfm_ba/") + DATASET_DIR + "/point_cloud_" + to_string(ncameras) + "_cameras.ply", tie_points_colors);
 
         // Запуск Bundle Adjustment
 #if ENABLE_BA
@@ -368,7 +370,7 @@ TEST (SFM, ReconstructNViews) {
 #endif
 
         generateTiePointsCloud(tie_points, tracks, keypoints, imgs, aligned, cameras, ncameras, tie_points_and_cameras, tie_points_colors);
-        phg::exportPointCloud(tie_points_and_cameras, std::string("data/debug/test_sfm_ba/") + DATASET_DIR + "/point_cloud_" + to_string(ncameras) + "_cameras_ba.ply", tie_points_colors);
+        phg::exportPointCloud(tie_points_and_cameras, std::string("/home/flint/s4/Photogrammetry/PhotogrammetryTasks2024/data/debug/test_sfm_ba/") + DATASET_DIR + "/point_cloud_" + to_string(ncameras) + "_cameras_ba.ply", tie_points_colors);
     }
 }
 
@@ -383,27 +385,58 @@ public:
                     const T* point_global,      // 3D точка: [3]  = {x, y, z}
                     T* residuals) const {       // невязка:  [2]  = {dx, dy}
         // TODO реализуйте функцию проекции, все нужно делать в типе T чтобы ceres-solver мог под него подставить как Jet (очень рекомендую посмотреть Jet.h - как класная статья из википедии!), так и double
+        T x = point_global[0];
+        T y = point_global[1];
+        T z = point_global[2];
 
         // translation[3] - сдвиг в локальную систему координат камеры
+        x -= camera_extrinsics[0];
+        y -= camera_extrinsics[1];
+        z -= camera_extrinsics[2];
 
         // rotation[3] - angle-axis rotation, поворачиваем точку point->p (чтобы перейти в локальную систему координат камеры)
         // подробнее см. https://en.wikipedia.org/wiki/Axis%E2%80%93angle_representation
         // (P.S. у камеры всмысле вращения три степени свободы)
+        T tmp_res[3];
+        T rotation[3];
+        rotation[0] = camera_extrinsics[3];
+        rotation[1] = camera_extrinsics[4];
+        rotation[2] = camera_extrinsics[5];
+        T tmp_point[3] = {x, y, z};
+        ceres::AngleAxisRotatePoint(rotation, tmp_point, tmp_res);
+        x = tmp_res[0];
+        y = tmp_res[1];
+        z = tmp_res[2];
 
-        // Проецируем точку на фокальную плоскость матрицы (т.е. плоскость Z=фокальная длина)
+        T k1 = camera_intrinsics[0];
+        T k2 = camera_intrinsics[1];
+        T f = camera_intrinsics[2];
+        T cx = camera_intrinsics[3];
+        T cy = camera_intrinsics[4];
+
+        x /= z;
+        y /= z;
 
 #if ENABLE_INSTRINSICS_K1_K2
         // k1, k2 - коэффициенты радиального искажения (radial distortion)
+        T r2 = x * x + y * y;
+        x *= (1.0 + k1 * r2 + k2 * r2 * r2);
+        y *= (1.0 + k1 * r2 + k2 * r2 * r2);
 #endif
 
         // Домножаем на f, тем самым переводя в пиксели
+        x *= f;
+        y *= f;
 
         // Из координат когда точка (0, 0) - центр оптической оси
         // Переходим в координаты когда точка (0, 0) - левый верхний угол картинки
         // cx, cy - координаты центра оптической оси (обычно это центр картинки, но часто он чуть смещен)
+        x += cx;
+        y += cy;
 
         // Теперь по спроецированным координатам не забудьте посчитать невязку репроекции
-
+        residuals[0] = x - observed_x;
+        residuals[1] = y - observed_y;
         return true;
         // TODO сверьте эту функцию с вашей реализацией проекции в src/phg/core/calibration.cpp (они должны совпадать)
     }
@@ -436,7 +469,7 @@ void runBA(std::vector<vector3d> &tie_points,
 
     // внутренние калибровочные параметры камеры: [5] = {k1, k2, f, cx, cy}
     // TODO: преобразуйте calib в блок параметров камеры (ее внутренних характеристик) для оптимизации в BA
-    double camera_intrinsics[5];
+    double camera_intrinsics[5] = {calib.k1_, calib.k2_, calib.f_, calib.cx_ + calib.width() * 0.5, calib.cy_ + calib.height() * 0.5};
     std::cout << "Before BA ";
     printCamera(camera_intrinsics);
 
@@ -577,6 +610,11 @@ void runBA(std::vector<vector3d> &tie_points,
     printCamera(camera_intrinsics);
     // TODO преобразуйте параметры камеры в обратную сторону, чтобы последующая резекция учла актуальное представление о пространстве:
     // calib.* = camera_intrinsics[*];
+    calib.k1_ = camera_intrinsics[0];
+    calib.k2_ = camera_intrinsics[1];
+    calib.f_ = camera_intrinsics[2];
+    calib.cx_ = camera_intrinsics[3] - calib.width() * 0.5;
+    calib.cy_ = camera_intrinsics[4] - calib.height() * 0.5;
 
     ASSERT_NEAR(calib.f_ , DATASET_F, 0.2 * DATASET_F);
     ASSERT_NEAR(calib.cx_, 0.0, 0.3 * calib.width());
@@ -650,7 +688,29 @@ void runBA(std::vector<vector3d> &tie_points,
 
             if (ENABLE_OUTLIERS_FILTRATION_COLINEAR && ENABLE_BA) {
                 // TODO выполните проверку случая когда два луча почти параллельны, чтобы не было странных точек улетающих на бесконечность (например чтобы угол был хотя бы 2.5 градуса)
-                // should_be_disabled = true;
+                for (size_t cj = 0; cj < track.img_kpt_pairs.size(); ++cj) {
+                    if (ci != cj) {
+                        matrix3d R2; vector3d camera_origin2;
+                        phg::decomposeUndistortedPMatrix(R2, camera_origin2, cameras[camera_id]);
+                        vector3d track_in_camera = R * (track_point - camera_origin);
+                        vector3d track_in_camer2 = R2 * (track_point - camera_origin2);
+                        
+                        double cosine = cos(track_in_camera.dot(track_in_camer2) / (cv::norm(track_in_camera) * cv::norm(track_in_camer2)));
+                        double angle;
+                        if (cosine > 1.0) {
+                            angle = 0.0;
+                        }
+                        else if (cosine < -1.0) {
+                            angle = CV_PI;
+                        } else {
+                            angle = acos(cosine);
+                        }
+                        angle = angle * 180 / CV_PI;
+                        if (angle < 2.5) {
+                            should_be_disabled = true;
+                        }
+                    }
+                }
             }
 
             {
