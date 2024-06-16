bcf788ad12f34e43cda96985716a20253b7c3745
Igor Loginov <igor.loginov.plus@yandex.ru>
2024-06-16 16:59:24 +0300

    task05 - some CI changes

--- .github/scripts/install_opencv451.sh
+++ .github/scripts/install_opencv451.sh
@@ -8,4 +8,3 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_LIST=features2d,highgui,flann,calib3d -DWITH_OPENEXR=ON -DBUILD_EXAMPLES=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=/opt/opencv451  ..
 njobs=`lscpu -p | egrep -v '^#' | sort -u -t, -k 2,4 | wc -l` # https://stackoverflow.com/a/23378780
 make -j{njobs} # njobs - число потоков которое будет использоваться для компиляции (по числу ядер)
-sudo make install
