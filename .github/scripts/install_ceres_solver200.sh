d730ce6eb27de0bb21ff37fe13c2242cf4ba9c35
Igor Loginov <igor.loginov.plus@yandex.ru>
2024-06-16 17:42:08 +0300

    task05 fix CI

--- .github/scripts/install_ceres_solver200.sh
+++ .github/scripts/install_ceres_solver200.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo apt install libunwind-dev libgoogle-glog-dev libgflags-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev
 wget https://github.com/ceres-solver/ceres-solver/archive/2.0.0.zip
 unzip 2.0.0.zip
 cd ceres-solver-2.0.0
